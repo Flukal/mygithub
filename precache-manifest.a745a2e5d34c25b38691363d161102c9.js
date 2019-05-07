64656995b6518b80d695b2453580f864351d1991
Flukal <fluviobass@gmail.com>
2019-05-07 21:51:58 +0200

    Updates

---
+++ precache-manifest.a745a2e5d34c25b38691363d161102c9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "19532f8263a12aa84f96",
+    "url": "/mygithub/static/js/runtime~main.19532f82.js"
+  },
+  {
+    "revision": "d320b4d05e6fa6f0615b",
+    "url": "/mygithub/static/js/main.d320b4d0.chunk.js"
+  },
+  {
+    "revision": "09d1282818da048d61e0",
+    "url": "/mygithub/static/js/2.09d12828.chunk.js"
+  },
+  {
+    "revision": "c453070da0a7f27983b5058f2ff34d24",
+    "url": "/mygithub/index.html"
+  }
+];
